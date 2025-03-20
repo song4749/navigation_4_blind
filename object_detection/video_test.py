9cecd8f64c1a4582606b5ec2e176bf740532d627
song4749 <lone4749@naver.com>
2025-03-20 16:38:37 +0900

    2

--- object_detection/video_test.py
+++ object_detection/video_test.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a pretrained YOLO11n model
-model = YOLO("object_detection/best.onnx")
+model = YOLO("object_detection\obstacle_test.onnx")
 
 # Run inference on 'bus.jpg' with arguments
 model.predict("KakaoTalk_20250317_094229683.mp4", save=True, imgsz=640, conf=0.5)
